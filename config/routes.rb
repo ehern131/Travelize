154f0d72e6e506ebd1be9eaa704dab506e65adc8
Erick Hernandez <ehern131@fiu.edu>
2016-11-23 14:54:04 -0500

    Remove Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
+  root 'users#welcome'
+
   resources :venues
   resources :trips
   resources :users
   resources :categories
+
+  get '/signup' => 'users#new'
+    post '/users' => 'users#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
